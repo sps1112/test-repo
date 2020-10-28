3e7b9ed1b536e1983709d48bea9f0a763db06fbb
sps1112 <sidps1112@gmail.com>
2020-10-28 23:30:48 +0530

    Added a Test file

---
+++ Test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+   int a, b, c, d;
+   a = 2;
+   b = 3;
+   c = 4;
+   d = a + (b * c);
+   std::cout << "Final value is " << d << "\n"
+             << std::endl;
+   return 0;
+}
